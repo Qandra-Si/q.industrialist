4b1aad5cceff05662f9dcc91afb1271fa5a75903
Qandra Si <qandra.si@gmail.com>
2020-09-05 09:34:57 +0300

    не были видны русские символы из ingame названий

--- render_html.py
+++ render_html.py
@@ -43,7 +43,7 @@ def __dump_header(glf, header_name):
 <!doctype html>
 <html lang="ru">
  <head>
- <!-- <meta charset="utf-8"> -->
+ <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1">
 <style type="text/css">
