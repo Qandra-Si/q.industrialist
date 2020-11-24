62f3b34f862b02e7e2640550dbfe08f6c7458a7a
Aliaksandr Klimasheuski <gladiator1988@gmail.com>
2020-11-24 15:08:21 +0300

    fixed file naming

--- eve/domain/__init__.py
+++ eve/domain/__init__.py
@@ -1,5 +1,5 @@
 from eve.domain._asset import Asset
 from eve.domain._inventory_location import InventoryLocation
-from eve.domain.market_price import MarketPrice
+from eve.domain._market_price import MarketPrice
 from eve.domain._type_info import TypeInfo
 from eve.domain._market_group import MarketGroup
