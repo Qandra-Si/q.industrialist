e4369f0b2c1c3272f1211defe660c86127c97ef6
Qandra Si <qandra.si@gmail.com>
2022-01-12 12:23:00 +0300

    Олег сообщил настройки торгового терминала для 4-h

--- php_interface/qi_market_4hwwf.php
+++ php_interface/qi_market_4hwwf.php
@@ -1,4 +1,8 @@
 ﻿<?php
+$IMPORT_PRICE_TO_TRADE_HUB = 160; // например, цена импорта 1куб.м. из Jita в Querious была 866 ISK
+$DEFAULT_PROFIT = 0.15; // 15%
+$BROKERS_FEE = 0.025; // брокерская комиссия
+$TRADE_HUB_TAX = 0.036; // sales tax, налог на структуре
 $CORPORATION_ID = 98615601; // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
 $TRADE_HUB_ID = 1035466617946; // PZ: 1034323745897, Nisuwa: 60015073, 4-HWWF: 1035466617946, NSI-MW: 1022822609240, FH-TTC: 1036176306541
 
