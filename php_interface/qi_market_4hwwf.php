c6cc64862ec87e671a5cfebed912338632c05717
Qandra Si <qandra.si@gmail.com>
2022-01-13 15:13:35 +0300

    поправка для vots

--- php_interface/qi_market_4hwwf.php
+++ php_interface/qi_market_4hwwf.php
@@ -3,7 +3,7 @@ $IMPORT_PRICE_TO_TRADE_HUB = 160; // например, цена импорта 1
 $DEFAULT_PROFIT = 0.15; // 15%
 $BROKERS_FEE = 0.025; // брокерская комиссия
 $TRADE_HUB_TAX = 0.036; // sales tax, налог на структуре
-$CORPORATION_IDs = array(98677876,98677876); // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
+$CORPORATION_IDs = array(98677876,98615601); // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
 $TRADE_HUB_ID = 1035466617946; // PZ: 1034323745897, Nisuwa: 60015073, 4-HWWF: 1035466617946, NSI-MW: 1022822609240, FH-TTC: 1036176306541
 
 include_once 'qi_trade_hub.php';
