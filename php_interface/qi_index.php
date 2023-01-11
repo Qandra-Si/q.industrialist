91716deaa4d8a5aaa624c1863d5200380e827115
Qandra Si <qandra.si@gmail.com>
2023-01-12 00:17:38 +0300

    добавил два новых киоска с новыми настройками профита

--- php_interface/qi_index.php
+++ php_interface/qi_index.php
@@ -79,8 +79,10 @@ EOD;
  <a class="btn btn-success" href="/qi_market_nisuwa.php" role="button">Nisuwa Market</a>
  <a class="btn btn-success" href="/qi_market_nsimw.php" role="button">Malpais Market</a>
  <a class="btn btn-success" href="/qi_market_4hwwf.php" role="button">4H-WWF Market</a>
- <a class="btn btn-success" href="/qi_market_fhttc.php" role="button">FH-TTC Market</a><br>
- <br>
+ <a class="btn btn-success" href="/qi_market_fhttc.php" role="button">FH-TTC Market</a>
+ <a class="btn btn-success" href="qi_market_mj5f9.php" role="button">MJ-5F9 Market</a>
+ <a class="btn btn-success" href="qi_market_b2j5n.php" role="button">B2J-5N Market</a><br>
+<br>
 <?php
     $query = <<<EOD
 select distinct
