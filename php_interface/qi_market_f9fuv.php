437782c7b9769d6700bc014e4305ba99217b1486
Qandra Si <qandra.si@gmail.com>
2023-04-27 17:56:18 +0300

    f9

---
+++ php_interface/qi_market_f9fuv.php
@@ -0,0 +1,10 @@
+<?php
+$IMPORT_PRICE_TO_TRADE_HUB = 37; // https://evemaps.dotlan.net/jump/Rhea,555/F9-FUV:B2J-5N:MJ-5F9 : 6082/4863*30
+$DEFAULT_PROFIT = 0.15; // 15%
+$BROKERS_FEE = 0.0495; // брокерская комиссия
+$TRADE_HUB_TAX = 0.02; // sales tax, налог на структуре
+$CORPORATION_IDs = array(98677876); // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
+$TRADE_HUB_ID = 1036732971380;
+
+include_once 'qi_trade_hub.php';
+?>
