833508655eb0c1b34bc8fadad5fd0af60518b028
Qandra Si <qandra.si@gmail.com>
2023-05-02 23:27:40 +0300

    чушь получилась - 5% от jita price :)

--- php_interface/qi_market_jita.php
+++ php_interface/qi_market_jita.php
@@ -1,6 +1,6 @@
 <?php
 $IMPORT_PRICE_TO_TRADE_HUB = 363; // https://evemaps.dotlan.net/jump/Rhea,555/F9-FUV:B2J-5N:MJ-5F9 : 29477/4863*30*2
-$DEFAULT_PROFIT = 0.05; // 5%
+$DEFAULT_PROFIT = 0.00; // 0%
 $BROKERS_FEE = 0.02; // брокерская комиссия
 $TRADE_HUB_TAX = 0.036; // sales tax, налог на структуре
 $CORPORATION_IDs = array(98553333); // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
