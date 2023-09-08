901cf873b36b1a2fbdb7050da2aaffe1a600de5a
Qandra Si <qandra.si@gmail.com>
2023-09-08 18:50:46 +0300

    добавил маркет w5-205

---
+++ php_interface/qi_market_w5205.php
@@ -0,0 +1,10 @@
+<?php
+$IMPORT_PRICE_TO_TRADE_HUB = 26; // например, цена импорта 1куб.м. из Jita в Querious была 866 ISK
+$DEFAULT_PROFIT = 0.15; // 15%
+$BROKERS_FEE = 0.01; // брокерская комиссия
+$TRADE_HUB_TAX = 0.036; // sales tax, налог на структуре
+$CORPORATION_IDs = array(98677876); // R Initiative 4: 98615601, R Strike: 98553333, R Industry: 98677876
+$TRADE_HUB_ID = 1032853274346;
+
+include_once 'qi_trade_hub.php';
+?>
