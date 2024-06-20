dfc2bc0a5821b9eb088f8798a4a371bda9526f22
Qandra Si <qandra.si@gmail.com>
2024-06-20 14:36:16 +0300

    чуть ускорил скачивание картинок (когда их около 10'000, то процесс идёт крайне медленно)

--- offline_resources/image_export_collection/download_images.sh
+++ offline_resources/image_export_collection/download_images.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 while read -r line; do
  line=$(echo $line)
- curl http://imageserver.eveonline.com/Type/${line}_32.png --output ../../.q_industrialist/image_export_collection/Types/${line}_32.png
+ output="../../.q_industrialist/image_export_collection/Types/${line}_32.png"
+ if [[ -f $output ]]; then
+  echo "image ${line} exists"
+ else
+  echo "downloading image ${line}"
+  curl http://imageserver.eveonline.com/Type/${line}_32.png --output $output
+ fi
 done < download_images.txt
--- offline_resources/image_export_collection/download_images.sql
+++ offline_resources/image_export_collection/download_images.sql
@@ -1,4 +1,4 @@
 select sdet_type_id
 from qi.eve_sde_type_ids
 where sdet_created_at >= '2024-06-11'
-order by sdet_created_at;
+order by sdet_created_at desc;
